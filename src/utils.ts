a33b3a1eed6c61704241a75fa760d51a81c50bd7
PeacefulOtter <zankro67@gmail.com>
2022-06-20 18:56:45 +0200

    Fixed interpolated RGB when adding a color onto the gradient

--- src/utils.ts
+++ src/utils.ts
@@ -27,10 +27,15 @@ export const interpolateColors = (colors: TRGB[], i: number, x: number): TRGB =>
     if ( i === colors.length ) return { ...colors[i - 1], t: x }
     const c1 = colors[i - 1]
     const c2 = colors[i]
+    const d1 = x - c1.t;
+    const d2 = c2.t - x;
+    const D = d1 + d2
+    const f1 = 1 - d1 / D
+    const f2 = 1 - d2 / D
     return {
-        r: Math.round(c1.r * (1 - x) + c2.r * x),
-        g: Math.round(c1.g * (1 - x) + c2.g * x),
-        b: Math.round(c1.b * (1 - x) + c2.b * x),
+        r: c1.r * f1 + c2.r * f2,
+        g: c1.g * f1 + c2.g * f2,
+        b: c1.b * f1 + c2.b * f2,
         t: x
     };
 }
@@ -38,5 +43,6 @@ export const interpolateColors = (colors: TRGB[], i: number, x: number): TRGB =>
 export const getColorOnGradient = (colors: TRGB[], pos: number): TRGB => {
     let i: number = 0;
     while ( colors[i].t < pos && ++i < colors.length );
+    console.log(pos, i);
     return interpolateColors(colors, i, pos);
 }
